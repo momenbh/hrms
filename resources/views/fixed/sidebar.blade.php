a7af44f13e9f46fdf0dbb5e4be910927d286df82
momenbh <mdmomenbhuiya@gmail.com>
2022-06-27 13:44:03 +0600

    sidebar

--- resources/views/fixed/sidebar.blade.php
+++ resources/views/fixed/sidebar.blade.php
@@ -16,20 +16,20 @@
                     <span>Employees</span>
                 </li>
                 <li class="submenu">
-                    <a href="#" class="noti-dot"><i class="la la-user"></i> <span> Employees</span> <span class="menu-arrow"></span></a>
+                    <a href="#" class="noti-dot"><i class="la la-user"></i> <span>Departments</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="employees.html">All Employees</a></li>
-                        <li><a href="holidays.html">Holidays</a></li>
-                        <li><a href="leaves.html">Leaves (Admin) <span class="badge badge-pill bg-primary float-right">1</span></a></li>
-                        <li><a href="leaves-employee.html">Leaves (Employee)</a></li>
-                        <li><a href="leave-settings.html">Leave Settings</a></li>
+                        <li><a href="employees.html">All Departments</a></li>
+                        <li><a href="holidays.html">Designations</a></li>
+                         <li><a href="leaves-employee.html">Employees</a></li>
+                        <li><a href="leave-settings.html"> Manage Leave</a></li>
                         <li><a href="attendance.html">Attendance (Admin)</a></li>
-                        <li><a href="attendance-employee.html">Attendance (Employee)</a></li>
-                        <li><a href="departments.html">Departments</a></li>
-                        <li><a href="designations.html">Designations</a></li>
+                       <li><a href="attendance-employee.html">Attendance (Employee)</a></li>
+                       <li><a class="active" href="leaves.html">Leaves (Admin) <span class="badge badge-pill bg-primary float-right">1</span></a></li>
+                        <li><a href="departments.html">Notice</a></li>
+                        <li><a href="designations.html">Holidays</a></li>
                         <li><a href="timesheet.html">Timesheet</a></li>
                         <li><a href="shift-scheduling.html">Shift & Schedule</a></li>
-                        <li><a href="overtime.html">Overtime</a></li>
+                      
                     </ul>
                 </li>
                
@@ -41,14 +41,12 @@
                     <a href="#" class="noti-dot"><i class="la la-user"></i> <span> Employees</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
                         <li><a href="employees.html">All Employees</a></li>
-                        <li><a href="holidays.html">Holidays</a></li>
-                        <li><a href="leaves.html">Leaves (Admin) <span class="badge badge-pill bg-primary float-right">1</span></a></li>
-                        <li><a href="leaves-employee.html">Leaves (Employee)</a></li>
-                        <li><a href="leave-settings.html">Leave Settings</a></li>
-                        <li><a href="attendance.html">Attendance (Admin)</a></li>
+                        <li><a href="holidays.html"></a></li>
+                        <li><a href="timesheet.html">Apply Leave</a></li>
+                        <li><a href="timesheet.html">Payroll</a></li>
+                         <li><a href="leave-settings.html">Leave Settings</a></li>
                         <li><a href="attendance-employee.html">Attendance (Employee)</a></li>
-                        <li><a href="departments.html">Departments</a></li>
-                        <li><a href="designations.html">Designations</a></li>
+                        <li><a class="active" href="leaves.html">Leaves (Admin) <span class="badge badge-pill bg-primary float-right">1</span></a></li>
                         <li><a href="timesheet.html">Timesheet</a></li>
                         <li><a href="shift-scheduling.html">Shift & Schedule</a></li>
                         <li><a href="overtime.html">Overtime</a></li>
